35263748b9d9f9912dd2b994e8b4ac7fc4d86653
sonng17 <ndson1703@gmail.com>
2024-11-10 17:06:10 +0700

    create header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,3 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import SignIn from "./pages/SignIn";
-import Profile from "./pages/Profile";
-import SignUp from "./pages/SignUp";
-import About from "./pages/About";
-
 export default function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/sign-in" element={<SignIn />}></Route>
-        <Route path="/sign-up" element={<SignUp />}></Route>
-        <Route path="/about" element={<Profile />}></Route>
-        <Route path="/profile" element={<About />}></Route>
-      </Routes>
-    </BrowserRouter>
-  );
+  return <div></div>;
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,29 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import Profile from "./pages/Profile";
+import SignUp from "./pages/SignUp";
+import About from "./pages/About";
+import Header from "./components/Header.jsx";
+import Footer from "./components/Footer.jsx";
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/sign-in" element={<SignIn />}></Route>
+        <Route path="/sign-up" element={<SignUp />}></Route>
+        <Route path="/about" element={<Profile />}></Route>
+        <Route path="/profile" element={<About />}></Route>
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  </StrictMode>
+);
