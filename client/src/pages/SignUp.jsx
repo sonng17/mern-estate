6c87b7b8588b419595a9b70c0d7b6c68b9bea2d4
sonng17 <ndson1703@gmail.com>
2024-11-22 20:54:28 +0700

    complete sign in page and small fix

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
-
 export default function SignUp() {
   const [formData, setFormData] = useState({});
   const [error, setError] = useState(null);
@@ -30,17 +29,18 @@ export default function SignUp() {
       if (data.success === false) {
         setError(data.message);
         setLoading(false);
+      } else {
+        setLoading(false);
+        setError(null);
+        navigate("/sign-in");
+        console.log(data);
       }
-      setLoading(false);
-      setError(null);
-      navigate('/sign-in')
-      console.log(data);
     } catch (error) {
       setLoading(false);
       setError(error.message);
     }
   };
-  console.log(formData);
+  // console.log(formData);
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
