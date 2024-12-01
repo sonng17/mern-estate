ea04e5cafee1e9f58b3c208c89d6c85d3071b6ed
sonng17 <ndson1703@gmail.com>
2024-12-01 21:33:41 +0700

    add redux persist

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,6 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { store } from "./redux/store.js";
+import { persistor, store } from "./redux/store.js";
 import { Provider } from "react-redux";
 
 // import App from "./App.jsx";
@@ -13,19 +13,22 @@ import SignUp from "./pages/SignUp";
 import About from "./pages/About";
 import Header from "./components/Header.jsx";
 import Footer from "./components/Footer.jsx";
+import { PersistGate } from "redux-persist/integration/react";
 
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/sign-in" element={<SignIn />}></Route>
-        <Route path="/sign-up" element={<SignUp />}></Route>
-        <Route path="/about" element={<Profile />}></Route>
-        <Route path="/profile" element={<About />}></Route>
-      </Routes>
-      <Footer />
-    </BrowserRouter>
+    <PersistGate loading={null} persistor={persistor}>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/sign-in" element={<SignIn />}></Route>
+          <Route path="/sign-up" element={<SignUp />}></Route>
+          <Route path="/about" element={<Profile />}></Route>
+          <Route path="/profile" element={<About />}></Route>
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </PersistGate>
   </Provider>
 );
