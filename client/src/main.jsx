4f9895a8924e5a63cacc2cb9497342195d63b3c8
sonng17 <ndson1703@gmail.com>
2024-12-01 20:33:35 +0700

    add redux toolkit

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,9 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.jsx";
+import { store } from "./redux/store.js";
+import { Provider } from "react-redux";
+
+// import App from "./App.jsx";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
@@ -13,7 +15,7 @@ import Header from "./components/Header.jsx";
 import Footer from "./components/Footer.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
+  <Provider store={store}>
     <BrowserRouter>
       <Header />
       <Routes>
@@ -25,5 +27,5 @@ createRoot(document.getElementById("root")).render(
       </Routes>
       <Footer />
     </BrowserRouter>
-  </StrictMode>
+  </Provider>
 );
