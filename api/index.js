d94430d4ee749955f70c0ac07b8b84e2cb8ad648
sonng17 <ndson1703@gmail.com>
2024-12-10 17:34:24 +0700

    create listing api route

--- api/index.js
+++ api/index.js
@@ -3,7 +3,8 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
-import cookieParser from 'cookie-parser';
+import cookieParser from "cookie-parser";
+import listingRouter from "./routes/listing.route.js";
 
 //Connect db, config .env
 dotenv.config(); //config env
@@ -28,6 +29,7 @@ app.use(express.json()); //Auto phải có, để client gửi json as input khi
 app.use(cookieParser());
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+app.use("/api/listing", listingRouter);
 //-handling error middleware
 app.use((err, req, res, next) => {
   //tham so next de sang middleware tiep theo neu dung
