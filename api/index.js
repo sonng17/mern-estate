615ae2b2d0a0dfd6533f710e4685380b7e41a146
sonng17 <ndson1703@gmail.com>
2025-01-11 06:33:41 +0700

    add Cors

--- api/index.js
+++ api/index.js
@@ -7,6 +7,7 @@ import authRouter from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
 import listingRouter from "./routes/listing.route.js";
 import path from "path";
+import cors from "cors";
 
 //Connect db, config .env
 dotenv.config(); //config env
@@ -26,7 +27,17 @@ const app = express();
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000!");
-}); //
+});
+
+// Cấu hình CORS
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://mern-estate-web.vercel.app/"], // Thay bằng domain của frontend
+    methods: ["GET", "POST", "PUT", "DELETE"], // Các method được phép
+    allowedHeaders: ["Content-Type", "Authorization"], // Các header được phép
+  })
+);
+//
 
 //Create middleware to handle request: normal middleware, handling error middleware
 app.use(express.json()); //Auto phải có, để client gửi json as input khi dung post,get,... khi gui request, default server k cho phep nhan json
