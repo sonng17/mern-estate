f9fd2d5cbbc96774f0b702445ebed93e9c50b207
sonng17 <ndson1703@gmail.com>
2025-01-11 09:35:43 +0700

    product env fix 5

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -48,7 +48,7 @@ export const signin = async (req, res, next) => {
     res
       .cookie("access_token", token, {
         httpOnly: true,
-        secure: false,
+        secure: true,
         sameSite: "None",
       }) //Lưu vào cookies với trường access_token: token và các tùy chọn bảo mật
       .status(200)
