1212129091a3f6c28b87e828ab2f664af2e1f112
sonng17 <ndson1703@gmail.com>
2025-01-17 11:42:37 +0700

    add UI and fix bug

--- api/controllers/admin.controller.js
+++ api/controllers/admin.controller.js
@@ -141,7 +141,6 @@ export const deleteUser = async (req, res, next) => {
     return next(errorHandler(401, "You can only delete your own account!"));
   try {
     await User.findByIdAndDelete(req.params.id);
-    res.clearCookie("access_token");
     res.status(200).json("User has been deleted!");
   } catch (error) {
     next(error);
